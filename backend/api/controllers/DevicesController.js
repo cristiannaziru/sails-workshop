8e91d4c07b63f1cc7dbfb16911ba7eb89e5602ce
Cristian Naziru <cristian.naziru@gmail.com>
2020-04-14 14:00:32 +0300

    Session two changes

--- backend/api/controllers/DevicesController.js
+++ backend/api/controllers/DevicesController.js
@@ -1,11 +1,12 @@
 /**
- * UserController
+ * DevicesController
  *
  * @description :: Server-side actions for handling incoming requests.
  * @help        :: See https://sailsjs.com/docs/concepts/actions
  */
 
 module.exports = {
-    assign_user: function () {}
+  
+
 };
 
