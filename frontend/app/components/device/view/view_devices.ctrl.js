c7e791e9218c04a18be176f203cb6667aa9737a5
Cristian Naziru <cristian.naziru@gmail.com>
2020-05-19 15:33:32 +0300

    Session 7? Angular Directives

--- frontend/app/components/device/view/view_devices.ctrl.js
+++ frontend/app/components/device/view/view_devices.ctrl.js
@@ -9,10 +9,5 @@
       $scope.devices = response.data;
     });
 
-    $scope.deleteDevice = function (id) {
-      $http.delete(`/api/device/${id}`).then(() => {
-        $scope.devices = $scope.devices.filter((device) => device.id !== id);
-      });
-    };
   }
 })();
