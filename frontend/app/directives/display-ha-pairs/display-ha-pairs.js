f92d75e8b8334b8dbd39a053edcef575b27ec394
Alexandru Nicolescu (intern) <alexandru.nicolescu@3pillar.corp>
2020-09-04 13:11:41 +0300

    Added styling for AngularJS DataTable
    + display-ha-pairs.css styling for display-ha-pairs.html
    - Modified display-ha-pairs.html to change table class
    - Modified display_ha_pairs.js to generate a table for primary and secondary in every responsive row

--- frontend/app/directives/display-ha-pairs/display-ha-pairs.js
+++ frontend/app/directives/display-ha-pairs/display-ha-pairs.js
@@ -8,6 +8,7 @@
     /** @ngInject */
     function displayHaPairsDirective(DTOptionsBuilder, DTColumnBuilder) {
         function displayHaPairs() {
+            var info = "Abracadabra";
             var vm = this;
             vm.dtOptions = DTOptionsBuilder.newOptions()
                 .withOption('ajax', {
@@ -21,12 +22,47 @@
                 DTColumnBuilder.newColumn('name').withTitle('Name'),
                 DTColumnBuilder.newColumn('type').withTitle('Type'),
                 DTColumnBuilder.newColumn('pid').withTitle('ID'),
-                DTColumnBuilder.newColumn('primary.ip').withTitle('Primary -- IP').withClass('none'),
-                DTColumnBuilder.newColumn('primary.name').withTitle('Name').withClass('none'),
-                DTColumnBuilder.newColumn('primary.uuid').withTitle('UUID').withClass('none'),
-                DTColumnBuilder.newColumn('secondary.ip').withTitle('Secondary --  IP').withClass('none'),
-                DTColumnBuilder.newColumn('secondary.name').withTitle('Name').withClass('none'),
-                DTColumnBuilder.newColumn('secondary.uuid').withTitle('UUID').withClass('none')
+                DTColumnBuilder.newColumn('primary').renderWith(function (primary) {
+                    return `
+                    <table class="table table-bordered row-table">
+                        <thead>
+                            <tr>
+                                <th scope="col">IP</th>
+                                <th scope="col">Name</th>
+                                <th scope="col">UUID</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>
+                                ${primary.ip}
+                                </td>
+                                <td>
+                                ${primary.name}
+                                </td>
+                                <td>
+                                ${primary.uuid}
+                                </td>
+                            </tr>
+                    `
+                }).withClass('none'),
+                DTColumnBuilder.newColumn('secondary').renderWith(function (secondary) {
+                    return `
+                            <tr>
+                                <td>
+                                ${secondary.ip}
+                                </td>
+                                <td>
+                                ${secondary.name}
+                                </td>
+                                <td>
+                                ${secondary.uuid}
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    `
+                }).withClass('none')
             ];
         }
 
